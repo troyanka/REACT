abc8051ac8250eeb1be7e1d65722636579ec8ad1
troyanka <troyanka@gmail.com>
2019-01-03 16:56:32 +0200

    update

--- exc-shop/src/components/contact.jsx
+++ exc-shop/src/components/contact.jsx
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment} from 'react';
 
+// STOPED part 2 5:50 min
 class Contact extends Component {
-    state = {  }
-    render() { 
-        return ( <p>Contact</p> );
+    state = {}
+    render() {
+        return (
+            <Fragment>
+                <div className="row">
+                    <div className="col-12">
+                        <h1>Contact our company</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ipsam fugit nobis veritatis. Natus rerum earum hic enim fugit consequatur.</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                    </div>
+                </div>
+            </Fragment>
+        )
     }
 }
- 
+
 export default Contact;
