f7c44de136cd88aacd5a9f67f5d9c146e8c5f5b3
troyanka <troyanka@gmail.com>
2019-03-15 15:22:04 +0200

    pets app update

--- pet-adapter-masters/src/App.js
+++ pet-adapter-masters/src/App.js
@@ -1,82 +1,20 @@
+// Stopped at 2.59
 import React from "react";
-import {render} from "react-dom";
+import {render} from 'react-dom';
+import Results from './Results';
 
-import pf from 'petfinder-client';
-import Pet from "./components/Pet";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-})
 
 class App extends React.Component {
-
-  constructor(props) {
-    super();
-
-    this.state = {
-      pets: []
-    }
-  }
-
-
-  componentDidMount() {
-    petfinder.pet.find({autput: "full", location: "Seattle, WA"})
-    .then(data=>{
-      let pets;
-      if(data.petfinder.pets && data.petfinder.pets.pet){
-        if(Array.isArray(data.petfinder.pets.pet)){
-          pets = data.petfinder.pets.pet
-        }
-        else{
-          pets = [data.petfinder.pets.pet];
-        }      }
-      else{
-        []
-      }
-
-      this.setState({pets});
-
-    })
-  }
-
-
-
-  render() {
-    return ( <div >
-      <h1 > Adopt me! </h1> 
-
-
-  {/* // In this way we can see what we have on the state
-  // <pre>
-  //   <code>{JSON.stringify(this.state, null, 4)}</code>
-  // </pre> */}
-
-  <div>
-    {this.state.pets.map( pet => {
-      let breed;
-
-      if(Array.isArray(pet.breeds.breed)){
-        breed = pet.breeds.breed.join(', ');
-      }else{
-        breed = pet.breeds.breed;
-      }
-
-        return (
-        <Pet key={pet.id}
-          name={pet.name}
-          animal={pet.animal}
-          breed={breed}
-          media={pet.media}
-          location={`${pet.contact.city}, ${pet.contact.state}`}/>
-          )
-
-      }) 
-    }
- </div>
+  render() { 
+    return ( 
+      <div>
+        <h1>Adopt me!</h1>
+        <Results/>
       </div>
-    );
+     );
   }
+}
+ 
+export default App;
 
-};
 render(React.createElement(App), document.getElementById('root'));
--- pet-adapter-masters/src/dist/App.d36a57b6.js
+++ pet-adapter-masters/src/dist/App.d36a57b6.js
@@ -26076,12 +26076,15 @@ function (_React$Component) {
 
 var _default = Pet;
 exports.default = _default;
-},{"react":"node_modules/react/index.js","react-dom":"node_modules/react-dom/index.js"}],"App.js":[function(require,module,exports) {
+},{"react":"node_modules/react/index.js","react-dom":"node_modules/react-dom/index.js"}],"Results.js":[function(require,module,exports) {
 "use strict";
 
-var _react = _interopRequireDefault(require("react"));
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
 
-var _reactDom = require("react-dom");
+var _react = _interopRequireDefault(require("react"));
 
 var _petfinderClient = _interopRequireDefault(require("petfinder-client"));
 
@@ -26112,24 +26115,24 @@ var petfinder = (0, _petfinderClient.default)({
   secret: "a7e38a5b323c795df31d59e5d0b8f63b"
 });
 
-var App =
+var Results =
 /*#__PURE__*/
 function (_React$Component) {
-  _inherits(App, _React$Component);
+  _inherits(Results, _React$Component);
 
-  function App(props) {
+  function Results(props) {
     var _this;
 
-    _classCallCheck(this, App);
+    _classCallCheck(this, Results);
 
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(App).call(this));
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Results).call(this));
     _this.state = {
       pets: []
     };
     return _this;
   }
 
-  _createClass(App, [{
+  _createClass(Results, [{
     key: "componentDidMount",
     value: function componentDidMount() {
       var _this2 = this;
@@ -26158,7 +26161,9 @@ function (_React$Component) {
   }, {
     key: "render",
     value: function render() {
-      return _react.default.createElement("div", null, _react.default.createElement("h1", null, " Adopt me! "), _react.default.createElement("div", null, this.state.pets.map(function (pet) {
+      return _react.default.createElement("div", {
+        className: "search"
+      }, _react.default.createElement("div", null, this.state.pets.map(function (pet) {
         var breed;
 
         if (Array.isArray(pet.breeds.breed)) {
@@ -26179,12 +26184,71 @@ function (_React$Component) {
     }
   }]);
 
-  return App;
+  return Results;
 }(_react.default.Component);
 
 ;
+var _default = Results;
+exports.default = _default;
+},{"react":"node_modules/react/index.js","petfinder-client":"node_modules/petfinder-client/index.js","./components/Pet":"components/Pet.jsx"}],"App.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = require("react-dom");
+
+var _Results = _interopRequireDefault(require("./Results"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var App =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(App, _React$Component);
+
+  function App() {
+    _classCallCheck(this, App);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(App).apply(this, arguments));
+  }
+
+  _createClass(App, [{
+    key: "render",
+    value: function render() {
+      return _react.default.createElement("div", null, _react.default.createElement("h1", null, "Adopt me!"), _react.default.createElement(_Results.default, null));
+    }
+  }]);
+
+  return App;
+}(_react.default.Component);
+
+var _default = App;
+exports.default = _default;
 (0, _reactDom.render)(_react.default.createElement(App), document.getElementById('root'));
-},{"react":"node_modules/react/index.js","react-dom":"node_modules/react-dom/index.js","petfinder-client":"node_modules/petfinder-client/index.js","./components/Pet":"components/Pet.jsx"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"react":"node_modules/react/index.js","react-dom":"node_modules/react-dom/index.js","./Results":"Results.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -26211,7 +26275,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56846" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50194" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
