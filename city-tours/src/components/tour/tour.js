cb2e632c5ba593d22c08f58951cf54cd8aeda31e
Victoria Boymistr <Victoria.B@Tenengroup.com>
2019-07-04 10:22:58 +0300

    update react projects

--- city-tours/src/components/tour/tour.js
+++ city-tours/src/components/tour/tour.js
@@ -1,41 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import "./tour.scss";
 
-class Tour extends Component {
-  state = {};
-  render() {
-    return (
-      <article className='tour'>
-        <div className='img-container'>
-          <img
-            width='200'
-            src='https://images.pexels.com/photos/2278885/pexels-photo-2278885.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-            alt=''
-          />
-          <span className='close-btn'>
-            <i className='fas fa-window-close' />
+const Tour = props => {
+  let { img, city, name, info } = props.tourDetails;
+
+  return (
+    <article className='tour'>
+      <div className='img-container'>
+        <img src={img} alt='' />
+        <span className='close-btn'>
+          <i className='fas fa-window-close' />
+        </span>
+      </div>
+      <div className='tour-info'>
+        <h3>{city}</h3>
+        <h4>{name}</h4>
+        <h5>
+          info{" "}
+          <span>
+            <i className='fas fa-caret-square-down' />
           </span>
-        </div>
-        <div className='tour-info'>
-          <h3>City</h3>
-          <h4>Name</h4>
-          <h5>
-            info{" "}
-            <span>
-              <i className='fas fa-caret-square-down' />
-            </span>
-          </h5>
-          <p>
-            Tempor officia reprehenderit veniam ipsum elit nisi do. Labore
-            adipisicing officia adipisicing ex ad eu ipsum velit Lorem. Nulla
-            aliqua ipsum mollit eu ea anim consectetur Lorem velit enim occaecat
-            adipisicing. Aliqua ipsum laborum est laborum sit non proident
-            adipisicing excepteur laborum sint nisi eu aliquip.
-          </p>
-        </div>
-      </article>
-    );
-  }
-}
+        </h5>
+        <p>{info}</p>
+      </div>
+    </article>
+  );
+};
 
 export default Tour;
--- city-tours/src/components/tourList/index.js
+++ city-tours/src/components/tourList/index.js
@@ -3,10 +3,53 @@ import "./tourList.scss";
 import Tour from "../tour/tour";
 
 class TourList extends Component {
+  state = {
+    tours: [
+      {
+        id: 1,
+        city: "new york",
+        img:
+          "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        name: "new york bridge tour",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      },
+      {
+        id: 2,
+        city: "paris",
+        img:
+          "https://images.pexels.com/photos/338515/pexels-photo-338515.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        name: "paris lights tour",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      },
+      {
+        id: 3,
+        city: "london",
+        img:
+          "https://images.pexels.com/photos/427679/pexels-photo-427679.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        name: "london royal palace tour",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      },
+      {
+        id: 4,
+        city: "tokyo",
+        img:
+          "https://images.pexels.com/photos/1727627/pexels-photo-1727627.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        name: "tokyo sushi tour",
+        info:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      }
+    ]
+  };
+
   render() {
     return (
       <section className='tourList'>
-        <Tour />
+        {this.state.tours.map(tour => (
+          <Tour key={tour.id} tourDetails={tour} />
+        ))}
       </section>
     );
   }
