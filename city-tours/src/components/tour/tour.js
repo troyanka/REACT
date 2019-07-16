aec163d1a4b03cf34ff5a2b9b24dc655ff001282
Victoria Boymistr <Victoria.B@Tenengroup.com>
2019-07-16 14:53:20 +0300

    react project update

--- city-tours/src/components/tour/tour.js
+++ city-tours/src/components/tour/tour.js
@@ -1,44 +1,37 @@
 import React, { useState, Component } from "react";
+import ReactDOM from "react-dom";
 import "./tour.scss";
 
-class Tour extends Component {
-  state = {
-    showInfo: false
+function Tour(props) {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const handelInfo = () => {
+    setShowInfo(!showInfo);
   };
 
-  // const { showInfo, setShowInfo } = useState();
-
-  handelInfo = () => {
-    // console.log(this.state.showInfo);
-    this.setState({ showInfo: !this.state.showInfo });
-    // setShowInfo(!showInfo);
-  };
-
-  render() {
-    const { img, city, name, info, id } = this.props.tourDetails;
-    const { removeTour } = this.props;
-    return (
-      <article className='tour'>
-        <div className='img-container'>
-          <img src={img} alt='' />
-          <span className='close-btn' onClick={() => removeTour(id)}>
-            <i className='fas fa-window-close' />
+  const { img, city, name, info, id } = props.tourDetails;
+  const { removeTour } = props;
+  return (
+    <article className='tour'>
+      <div className='img-container'>
+        <img src={img} alt='' />
+        <span className='close-btn' onClick={() => removeTour(id)}>
+          <i className='fas fa-window-close' />
+        </span>
+      </div>
+      <div className='tour-info'>
+        <h3>{city}</h3>
+        <h4>{name}</h4>
+        <h5>
+          info{" "}
+          <span onClick={() => handelInfo()}>
+            <i className='fas fa-caret-square-down' />
           </span>
-        </div>
-        <div className='tour-info'>
-          <h3>{city}</h3>
-          <h4>{name}</h4>
-          <h5>
-            info{" "}
-            <span onClick={this.handelInfo}>
-              <i className='fas fa-caret-square-down' />
-            </span>
-          </h5>
-          {this.state.showInfo && <p>{info}</p>}
-        </div>
-      </article>
-    );
-  }
+        </h5>
+        {showInfo && <p>{info}</p>}
+      </div>
+    </article>
+  );
 }
 
 export default Tour;
