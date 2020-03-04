b6c04a882a23c4cb63acc2d8edb34a1d956d33bb
Victoria Boymistr <Victoria.B@Tenengroup.com>
2020-03-04 19:07:08 +0200

    some updates

--- UDEMY/react-complete-guide/src/App.js
+++ UDEMY/react-complete-guide/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <h1>Hi hkjh</h1>
+        <Person name='Vika1' age='29' />
+        <Person name='Linda' age='3' />
+        <Person name='Boris' age='32' />
+        <Person name='Michelle' age='2' />
       </div>
     );
   }
