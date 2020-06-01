8dc210f8948dd0ee67b703c85daf9e74dec62b20
troyanka <troyanka@gmail.com>
2020-06-01 10:03:50 +0300

    update

--- UDEMY/react-complete-guide/src/App.js
+++ UDEMY/react-complete-guide/src/App.js
@@ -1,19 +1,135 @@
 import React, { Component } from "react";
 import "./App.css";
+//import Radium, {StyleRoot} from 'radium';
+// import styled from 'styled-components';
 import Person from "./Person/Person";
 
-class App extends Component {
-  render() {
-    return (
+// const StyledButton = styled.button`
+// background-color: ${props=> props.alt ? 'red' : 'green'}; 
+// color: white;
+// font: inherit;
+// border: 1px solid blue;
+// padding: 8px;
+// cursor: pointer;
+
+// &:hover {
+//    background-color: ${props=> props.alt ? 'salmon' : 'lightgreen'}; 
+//    color: black;
+//     }
+// `;
+
+
+class app extends Component {
+  state = { persons: [{
+    id: 1,
+    name: 'Max',
+    age: 28
+  },
+  {
+    id: 2,
+    name: 'Manu',
+    age: 29
+  },
+  {
+    id: 3,
+    name: 'Stephany',
+    age: 26
+  }
+],
+otherState: 'some another value',
+showPersons: true }
+
+
+togglePersonsHandler  = () => {
+  const show = this.state.showPersons;
+  this.setState({showPersons: !show});
+}
+
+nameChangedHandler = (event, id) =>{
+  const personIndex = this.state.persons.findIndex(person => person.id === id);
+  const person = {...this.state.persons[personIndex]};
+  //const person = Object.assign({}, this.state.persons[personIndex]); //different approach
+  person.name = event.target.value;
+
+  let persons = [...this.state.persons];
+
+  console.log(personIndex);
+  persons[personIndex] = person;
+
+
+   console.log(persons);
+
+   this.setState({persons})
+}
+
+
+deletePersonHandler = (personIndex) =>{
+// updating state immutably
+// const persons = this.state.persons; //creates pointer to the original one
+// const persons = this.state.persons.splice(); //copying the array 
+const persons = [...this.state.persons]; //spread the array - ES6 approach
+persons.splice(personIndex, 1);
+this.setState({persons})
+} 
+
+  render() { 
+// const style = {
+//   backgroundColor: 'green',  
+//   color: 'white',
+//   font: 'inherit',
+//   border: '1px solid blue',
+//   padding: '8px',
+//   cursor: 'pointer',
+
+
+//   // To use it we have to add Radium
+// //   ':hover': {
+// //  backgroundColor: "lightcoral",
+// //  color: 'black'
+// //   }
+// };
+
+    let persons = null;
+
+    if (this.state.showPersons){
+     persons = ( <div>
+            {
+              this.state.persons.map((person, index)=>
+                <Person key={person.id} name={person.name} age={person.age}
+                changed={event=>this.nameChangedHandler(event, person.id)} click={()=>this.deletePersonHandler(index)}/>
+              )
+            }</div>)
+
+      style.backgroundColor = 'red';      
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2){
+       classes.push('red');
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
+   }
+
+
+  
+
+    return ( 
+    
+      
       <div className='App'>
-        <h1>Hi hkjh</h1>
-        <Person name='Vika1' age='29' />
-        <Person name='Linda' age='3' />
-        <Person name='Boris' age='32' />
-        <Person name='Michelle' age='2' />
-      </div>
+     <p className={classes.join(' ')} >This is really working</p>
+    {/* <StyledButton  alt={this.state.showPersons} onClick={this.togglePersonsHandler} >Toggle Persons</StyledButton> */}
+    <button  alt={this.state.showPersons} onClick={this.togglePersonsHandler} >Toggle Persons</button>
+
+
+    {persons}
+    
+  </div>
+ 
     );
   }
 }
-
-export default App;
+ 
+export default app;
+// export default Radium(app);
--- UDEMY/react-complete-guide/src/Person/Person.js
+++ UDEMY/react-complete-guide/src/Person/Person.js
@@ -1,11 +1,43 @@
 import React from "react";
+// import Radium from 'radium';
+import styled from 'styled-components';
+// import "./Person/Person.css";
 
-const person = ({ name, age }) => {
+
+const StyledDiv = styled.div`
+width: 60%;
+margin:  auto;
+border: 1px solid #eee;
+box-shadow: 0 2px 3px #ccc;
+padding: 16px;
+text-align: center;
+
+@media (min-width: 500px){width: 450px;}`;
+
+//functional component
+// stateless component, dumn
+const Person = (props) => {
+
+// const style = {
+//   '@media (min-width: 500px)' : { width: '450px'}
+// };
+
+
   return (
-    <p>
-      I`m a {name} I and I`am {age}!
+    // <div className="Person" style={style}>
+
+
+    <StyledDiv>
+
+
+    <p onClick={() => props.click(40)}>I`m {props.name} I and I`am {props.age}!
     </p>
+    {props.children && <p>{props.children}</p>}
+    <input type="text" onChange={event=> props.changed(event)} value={props.name}/>
+
+   </StyledDiv>
   );
 };
 
-export default person;
+export default Person;
+// export default Radium(Person);
