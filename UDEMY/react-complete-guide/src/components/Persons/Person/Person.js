63e57fc8fe6fada7ec257ea1e6dcdd2b411fb1cc
troyanka <troyanka@gmail.com>
2020-09-05 20:26:31 +0300

    projects from udemy

--- UDEMY/react-complete-guide/src/components/Persons/Person/Person.js
+++ UDEMY/react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,54 +1,24 @@
-import React from "react";
-// import Radium from 'radium';
+import React, { Component } from "react";
 // import styled from 'styled-components';
-import classes from './Person.css'
-// import "./Person/Person.css";
+import classes from "./Person.css";
 
+class Person extends Component {
+  render() {
+    const { click, name, children, age, changed } = this.props;
+    return (
+      // <StyledDiv>
 
-// const StyledDiv = styled.div`
-// width: 60%;
-// margin:  auto;
-// border: 1px solid #eee;
-// box-shadow: 0 2px 3px #ccc;
-// padding: 16px;
-// text-align: center;
+      <div className={classes.Person}>
+        <p onClick={click}>
+          I`m {name} I and I`am {age}!
+        </p>
+        {children && <p>{children}</p>}
+        <input type='text' onChange={changed} value={name} />
 
-// @media (min-width: 500px){width: 450px;}`;
-
-//functional component
-// stateless component, dumn
-
-
-const Person = (props) => {
-
-// const style = {
-//   '@media (min-width: 500px)' : { width: '450px'}
-// };
-
-
-// const rnd = Math.random();
-// if(rnd > 0.7){
-//  throw new Error('something went wrong');
-// }
-
-  return (
-    // <div className="Person" style={style}>
-
-
-    // <StyledDiv>
-
-
-    <div className={classes.Person}>
-
-    <p onClick={() => props.click(40)}>I`m {props.name} I and I`am {props.age}!
-    </p>
-    {props.children && <p>{props.children}</p>}
-    <input type="text" onChange={event=> props.changed(event)} value={props.name}/>
-
-   {/* </StyledDiv> */}
-   </div>
-  );
-};
+        {/* </StyledDiv> */}
+      </div>
+    );
+  }
+}
 
 export default Person;
-// export default Radium(Person);
--- UDEMY/react-complete-guide/src/containers/App.js
+++ UDEMY/react-complete-guide/src/containers/App.js
@@ -1,47 +1,81 @@
-import React, {
-  Component
-} from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 //import Radium, {StyleRoot} from 'radium';
 // import styled from 'styled-components';
-import Persons from '../components/Persons/Persons'
+import Persons from '../components/Persons/Persons';
 import classes from './App.css';
-import Cockpit from "../components/Cockpit/Cockpit";
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class app extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constractor');
+  }
+
   state = {
-    persons: [{
+    persons: [
+      {
         id: 1,
         name: 'Max',
-        age: 28
+        age: 28,
       },
       {
         id: 2,
         name: 'Manu',
-        age: 29
+        age: 29,
       },
       {
         id: 3,
         name: 'Stephany',
-        age: 26
-      }
+        age: 26,
+      },
     ],
     otherState: 'some another value',
-    showPersons: true
+    showPersons: true,
+    showCocpit: true,
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
   }
 
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    //what is compared here is the pointer of the arrays
+    if (nextProps.persons !== this.props.persons) {
+      return false;
+    } else {
+      return true;
+    }
+    //return true;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('[App.js] componentDidUpdate');
+  }
+
+  // componentWillMount() {
+  //   console.log("[App.js] componentWillMount");
+  // }
 
   togglePersonsHandler = () => {
     const show = this.state.showPersons;
     this.setState({
-      showPersons: !show
+      showPersons: !show,
     });
-  }
+  };
 
   nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(person => person.id === id);
+    const personIndex = this.state.persons.findIndex(
+      (person) => person.id === id
+    );
     const person = {
-      ...this.state.persons[personIndex]
+      ...this.state.persons[personIndex],
     };
     //const person = Object.assign({}, this.state.persons[personIndex]); //different approach
     person.name = event.target.value;
@@ -50,38 +84,59 @@ class app extends Component {
 
     persons[personIndex] = person;
     this.setState({
-      persons
-    })
-  }
-
+      persons,
+    });
+  };
 
   deletePersonHandler = (personIndex) => {
     // updating state immutably
     // const persons = this.state.persons; //creates pointer to the original one
-    // const persons = this.state.persons.splice(); //copying the array 
+    // const persons = this.state.persons.splice(); //copying the array
     const persons = [...this.state.persons]; //spread the array - ES6 approach
     persons.splice(personIndex, 1);
     this.setState({
-      persons
-    })
-  }
+      persons,
+    });
+  };
 
   render() {
+    console.log('[App js] render');
+
     let persons = null;
-    let btnClasses = [classes.button]
+    let btnClasses = [classes.button];
 
     if (this.state.showPersons) {
-      persons = <Persons persons={this.state.persons} cliked={this.deletePersonHandler} changed={this.nameChangedHandler}/> }
-
-
-      return (
-        <div className={classes.app}>
-          <Cockpit persons={this.state.persons} showPersons={this.state.showPersons} toggled={this.togglePersonsHandler}/>
-          {persons}
-        </div>
-
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          cliked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
       );
     }
-  }
 
-  export default app;
+    return (
+      <div className={classes.app}>
+        <button
+          onClick={() => {
+            this.setState({ showCocpit: false });
+          }}
+        >
+          Remove cocpit
+        </button>
+
+        {this.state.showCocpit ? (
+          <Cockpit
+            title={this.props.title}
+            personsLength={this.props.personsLength}
+            showPersons={this.state.showPersons}
+            toggled={this.togglePersonsHandler}
+          />
+        ) : null}
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default app;
